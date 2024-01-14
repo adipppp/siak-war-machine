03ebfc76832182726bfb2f5ab954d41b1b097eee
adipppp <adhif2004@gmail.com>
2024-01-14 18:47:40 +0700

    Add command status

--- index.js
+++ index.js
@@ -9,21 +9,32 @@ async function main() {
     output: process.stdout,
   });
   process.once("exit", () => rl.close());
-  
+
+  const options = new Set(["x", "l", "i", "r", "lg"]);
+
+  let blockCmd = false; // io-blocking flags
+  let blockStdout = false;
+
   let browser = await puppeteer.launch({
     headless: false,  
     executablePath: process.env.PATH_TO_CHROME_EXE,
   });
   let page = (await browser.pages())[0];
 
-  console.log('Enter "l" to login to SIAK-NG, "i" to navigate to IRS page, "r" to reload page, "lg" to logout, "x" to exit this program');
-  console.log("------------------------------------------------------------------------------------------------------------------------");
+  console.log("---------------------------");
+  console.log("Enter:");
+  console.log('"l" to login to SIAK-NG');
+  console.log('"i" to navigate to IRS page');
+  console.log('"r" to reload page');
+  console.log('"lg" to logout');
+  console.log('"x" to exit this program');
+  console.log("---------------------------");
+  console.log("Status: Ready");
+  console.log("---------------------------");
 
-  const options = new Set(["x", "l", "i", "r", "lg"]);
-  let blocked = false; // blocking algorithm
-
-  rl.on("line", async input => {
+  rl.on("line", async (input) => {
     if (input === "x") {
+      await browser.close();
       process.exit();
     }
     
@@ -34,11 +45,11 @@ async function main() {
       return;
     }
 
-    if (blocked) {
+    if (blockCmd) {
       return;
     }
 
-    blocked = true;
+    blockCmd = true;
 
     if (!browser.isConnected()) {
       browser = await puppeteer.launch({
@@ -50,26 +61,43 @@ async function main() {
       page = (await browser.pages())[0];
     }
 
-    blocked = false;
+    blockCmd = false;
+
+    if (!blockStdout) {
+      process.stdout.moveCursor(0, -2);
+      process.stdout.clearLine(0);
+      console.log("Status: Processing...");
+      process.stdout.moveCursor(0, -1);
+    }
+
+    blockStdout = true;
 
     switch (input) {
       case "l":
-        loginToSIAK(page);
+        await loginToSIAK(page);
         break;
       case "i":
-        page
+        await page
           .goto("https://academic.ui.ac.id/main/CoursePlan/CoursePlanEdit")
           .catch(e => null);
         break;
       case "r":
-        page.reload();
+        await page.reload();
         break;
       case "lg":
-        page
+        await page
           .goto("https://academic.ui.ac.id/main/Authentication/Logout")
           .catch(e => null);
         break;
     }
+
+    if (blockStdout) {
+      process.stdout.clearLine(0);
+      console.log("Status: Ready");
+      process.stdout.moveCursor(0, 1);
+    }
+
+    blockStdout = false;
   });
 }
 
